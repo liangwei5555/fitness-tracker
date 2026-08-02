004b6955fabd6a02ef211c4124be9078d9f30038
liangwei5555 <liangwei5555@users.noreply.github.com>
2026-08-02 23:19:22 +0800

    trigger redeploy

--- push.bat
+++ push.bat
@@ -1,6 +1,5 @@
 cd /d C:\Users\33125\Projects\fitness-tracker
-git add -A
-git commit -m "add app icon"
+git remote set-url origin https://liangwei5555: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@github.com/liangwei5555/fitness-tracker.git
 git push
 echo done
 pause
